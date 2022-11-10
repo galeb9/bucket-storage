644f6e5ad132f7e53b0f6b391e5abb7d7f7bafaf
Matej Grimšič <matej.grimsic@hudlajf.com>
2022-11-10 11:26:56 +0100

    feat: Angular app setup & added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import { NgbPaginationModule, NgbAlertModule, NgbModalModule  } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// components
+import { BasePopupComponent } from './components/UI/base-popup/base-popup.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasePopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgbPaginationModule,
+    // NgbAlertModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/UI/base-popup/base-popup.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BasePopupComponent } from './base-popup.component';
+
+describe('BasePopupComponent', () => {
+  let component: BasePopupComponent;
+  let fixture: ComponentFixture<BasePopupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BasePopupComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BasePopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
